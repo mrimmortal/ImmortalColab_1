87b1dc0587e7be0523d43342543ebbbeb2acbe3b
cloudyrathor <bhushan.ugale@gmail.com>
2024-07-10 15:38:46 +0530

    strng operation

--- Day_2_10_jul_2024.py
+++ Day_2_10_jul_2024.py
@@ -1,9 +1,25 @@
-'''Factorial of a non-negative integer, is multiplication of 
-all integers smaller than or equal to n. For example factorial 
-of 5 is 5*4*3*2*1 which is 120'''
+# '''Factorial of a non-negative integer, is multiplication of 
+# all integers smaller than or equal to n. For example factorial 
+# of 5 is 5*4*3*2*1 which is 120'''
 
-def factorial(num):
-    return 1 if (num == 1 or num == 0) else num * factorial(num-1)
+# def factorial(num):
+#     return 1 if (num == 1 or num == 0) else num * factorial(num-1)
 
-num = int(input("Enter the no : "))
-print(factorial(num))
+# num = int(input("Enter the no : "))
+# print(factorial(num))
+
+# ------------------------Play with strings-------------------
+
+x = "immortals world"
+y = "always"
+
+print(x)
+print(x[2])
+print(x[-2])
+print(x[0:3])
+print(x.split(" "))
+print(x.startswith("i"))
+print(x.startswith("x"))
+print(x.endswith("s"))
+print(x.replace("i","ee"))
+print(x.upper())
